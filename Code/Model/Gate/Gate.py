b2a24813ac15ae505733bf4713bd89406649e117
Mattia Bertacchini <matber2001@gmail.com>
2022-12-19 10:38:58 +0100

    File created: - Monitor.py (contains class Monitor) - Gate.py (contains class Gate)

---
+++ Code/Model/Gate/Gate.py
@@ -0,0 +1,36 @@
+import json
+
+
+class Gate:
+    """The class models gates used in the SmartParking"""
+
+    def __init__(self):
+        """Class only have 1 attribute: state (boolean).
+        If it's set to True """
+        self.state = False
+
+    def openGate(self):
+        """The method opens the gate: sets self.state to True"""
+        self.state = True
+
+    def closeGate(self):
+        """The method closes the gate: sets self.state to False"""
+        self.state = False
+
+    def isOpen(self):
+        """Returns True if gate is opened"""
+        return self.state is True
+
+    def isClosed(self):
+        """Returns True if gate is closed"""
+        return self.state is False
+
+    def switchState(self):
+        """Switches state: closed to opened, opened to closed"""
+        if self.isOpen():
+            self.closeGate()
+        else:
+            self.openGate()
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__())
--- Code/Model/Monitor/Monitor.py
+++ Code/Model/Monitor/Monitor.py
@@ -1,2 +1,30 @@
+import json
+
+
 class Monitor:
-    pass
+    """The class is a model for monitors inside the SmartParking"""
+
+    def __init__(self):
+        """self.display indicates what the monitor is displaying (String).
+        self.state indicates whether the monitor is turned on or off (boolean).
+        """
+        self.display = ""
+        self.state = False
+
+    def turnOn(self):
+        """The method turns on the monitor: changes self.state to on."""
+        self.state = True
+
+    def turnOff(self):
+        """The method turns off the monitor: changes self.state to off."""
+        self.state = False
+
+    def updateDisplay(self, string):
+        """The method sets what the monitor displays"""
+        if self.state is True:
+            self.display = string
+        else:
+            raise ValueError("Unable to update display: monitor is off.")
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__())
