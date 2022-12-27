91d9e74c895fcea784d59c32a912927cf43abdc5
Mattia Bertacchini <matber2001@gmail.com>
2022-12-27 19:23:27 +0100

    Parameter "descrizione" in Gate.py has been changed into "description"
    In Monitor.py the method updateDisplay() has been modified: now if the monitor is off it is turned on and the display is set to the string passed as parameter.
    
    In EntryMonitor.py has been added some TODO (i commenti sono in italiano per semplicità, sono dei TODO quindi andranno rimossi)

--- Code/Resource/CoAP/Monitor/EntryMonitor.py
+++ Code/Resource/CoAP/Monitor/EntryMonitor.py
@@ -7,10 +7,10 @@ from Code.Model.Monitor.Monitor import Monitor
 class EntryMonitor(resource.Resource):
     """The class represents the resource monitor for parking entrance"""
 
-    def __init__(self, localDB):
+    def __init__(self, monitorID, description, localDB):
         """The construct needs a LocalDB object in order to check parking slots and operate on them."""
         super().__init__()
-        self.Monitor = Monitor()
+        self.Monitor = Monitor(monitorID, description)
         self.localDB = localDB
 
     def getFirstFreeSlot(self):
@@ -23,10 +23,22 @@ class EntryMonitor(resource.Resource):
         return num, firstID
 
     def render_get(self, request):
+        """
+        TODO: return '<state>;<display>'
+        Ovvero deve ritornare lo stato (on off) e poi quello che sta mostrando (numero posti/ niente/ primo libero)
+        """
         pass
 
     def render_put(self, request):
+        """
+        TODO: paylaod_body='<state>;<display>' OPPURE (meglio) gestione da DataManager
+        1) La richiesta deve contenere lo stato nel quale lo si vuole settare e anche cosa deve mostrare.
+        2) DataManager, nel momento in cui leggo una modifica da plate reader invio al monitor cosa mostrare
+        """
         pass
 
     def render_post(self, request):
+        """
+        TODO: switch dello stato
+        """
         pass
