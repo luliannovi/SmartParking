2b9a0edc258d285b8d675199fd1123436d4d2391
Mattia Bertacchini <matber2001@gmail.com>
2023-01-12 17:55:41 +0100

    Monitor resource has been updated, needs to be completed and checked

--- Code/Resource/CoAP/Monitor/EntryMonitor.py
+++ Code/Resource/CoAP/Monitor/EntryMonitor.py
@@ -1,4 +1,8 @@
+import aiocoap
 import aiocoap.resource as resource
+import aiocoap.numbers as numbers
+
+from kpn_senml import *
 
 from Code.DataHandler.DataManager.Configuration.LocalDB import LocalDB
 from Code.Model.Monitor.Monitor import Monitor
@@ -10,24 +14,42 @@ class EntryMonitor(resource.Resource):
     def __init__(self, monitorID, description, localDB):
         """The construct needs a LocalDB object in order to check parking slots and operate on them."""
         super().__init__()
-        self.Monitor = Monitor(monitorID, description)
-        self.localDB = localDB
+        self.monitorID = monitorID
+        self.description = description
 
-    def getFirstFreeSlot(self):
-        """The method checks if any parking slot is available.
-        Raises errors if LocalDB methods checkParkingSlots() returns any errors.
-        Otherwise, returns (number of slots available, ID of first slot available)"""
-        check, num, firstID = self.localDB.checkParkingSlots()
-        if not check:
-            raise Exception(str(num))
-        return num, firstID
+        self.monitor = Monitor(monitorID, description)
+        self.localDB = localDB
+        # interface actuator
+        self.if_ = 'monitor'
+        # resource type
+        self.rt = 'it.resource.monitor'
+        # content type
+        self.ct = numbers.media_types_rev['application/senml+json']
+
+    # ------------------------------ DA CANCELLARE ------------------------------
+    # def getFirstFreeSlot(self):
+    #     """The method checks if any parking slot is available.
+    #     Raises errors if LocalDB methods checkParkingSlots() returns any errors.
+    #     Otherwise, returns (number of slots available, ID of first slot available)"""
+    #     check, num, firstID = self.localDB.checkParkingSlots()
+    #     if not check:
+    #         raise Exception(str(num))
+    #     return num, firstID
+    # ---------------------------------------------------------------------------‚
+
+    def buildSenMLJson(self):
+        state = self.monitor.state
+        display = self.monitor.display
+        pack = SenmlPack(self.monitor.monitorID)
+
 
     async def render_get(self, request):
         """
         TODO: return '<state>;<display>'
         Ovvero deve ritornare lo stato (on off) e poi quello che sta mostrando (numero posti/ niente/ primo libero)
         """
-        pass
+        payload_string = self.monitor.toJson()
+        return aiocoap.Message(content_format=self.ct, payload=f'{str(self.monitor.state)};display={str(self.monitor.display)}'.encode('utf-8'))
 
     async def render_put(self, request):
         """
