8c1c15504038668b94cd624cc78c9da30e78b82f
Mattia Bertacchini <matber2001@gmail.com>
2023-01-15 17:53:12 +0100

    Italian comments have been translated

--- Code/DataHandler/DataManager/Configuration/LocalDB.py
+++ Code/DataHandler/DataManager/Configuration/LocalDB.py
@@ -161,8 +161,7 @@ class LocalDB:
         
     def addParkingSlot(self, parkingSlot):
         """
-        riceve aggiornamento su un singolo parkingSlot, libero o occupato
-        con stringa riguardo alla targa
+        the method gets updates about a single parkingSlot (free/taken) with a string that contains the plate
         """
         try:
             with open(LocalDB.SUPPORTED_MEDIA[self.type], 'r+') as jsonStream:
