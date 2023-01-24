39c8713ca63638b3ea97a921d6781bcc4f5307c6
Mattia Bertacchini <matber2001@gmail.com>
2023-01-24 15:21:30 +0100

    Eduroam fa schifo

--- Test/CoAPserver.py
+++ Test/CoAPserver.py
@@ -3,10 +3,9 @@ from Code.Resource.CoAP.Monitor.EntryMonitor import EntryMonitor
 from Code.Resource.CoAP.Monitor.ExitMonitor import ExitMonitor
 from Code.Resource.CoAP.Gate.EntryGate import EntryGate
 from Code.Resource.CoAP.Gate.ExitGate import ExitGate
+from Code.Logging.Logger import loggerSetup
 
-logging.basicConfig(level=logging.INFO)
-logging.getLogger("coap-server").setLevel(logging.INFO)
-
+gateLogger = loggerSetup('gateLogger', 'Code/Logging/Gate/gate.log')
 
 def main():
     # Resource tree creation
@@ -14,11 +13,12 @@ def main():
     # define ids
     root.add_resource(['IoT/device/monitor/in'], EntryMonitor(monitorID='id1', description='entry monitor'))
     root.add_resource(['IoT/device/monitor/out'], ExitMonitor(monitorID='id2', description='exit monitor'))
-    root.add_resource(['IoT/actuator/gate/in'], EntryGate(gateID='id3', descrizione='entry gate'))
-    root.add_resource(['IoT/actuator/gate/out'], ExitGate(gateID='id4', descrizione='exit gate'))
+    root.add_resource(['IoT/actuator/gate/in'], EntryGate(gateID='id3', descrizione='entry gate', timesleep=10))
+    root.add_resource(['IoT/actuator/gate/out'], ExitGate(gateID='id4', descrizione='exit gate', timesleep=10))
 
     # Add WellKnownCore Resource to support the standard Resource Discovery
     asyncio.Task(aiocoap.Context.create_server_context(root, bind=('127.0.0.1', 5683)))
+    gateLogger.info('CoAP server started...')
     asyncio.get_event_loop().run_forever()
 
 if __name__ == '__main__':
