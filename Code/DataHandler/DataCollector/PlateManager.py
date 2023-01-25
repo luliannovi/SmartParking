55ae55f74667358470fc8119808ba84134669a43
Alessandro Pansera <46775926+allePansera@users.noreply.github.com>
2023-01-25 18:34:49 +0100

    Revert "Luli"

--- Code/DataHandler/DataCollector/PlateManager.py
+++ Code/DataHandler/DataCollector/PlateManager.py
@@ -3,14 +3,13 @@ import logging
 import time
 import aiocoap
 import paho.mqtt.client as mqtt
-
 from Code.DataHandler.DataCollector.MQTTBrokerParameters import MQTTBrokerParameters
 from Code.DataHandler.DataManager.CloudDialog.SendParkingStatus import SendParkingStatus
-
 from Code.DataHandler.DataManager.Configuration.LocalDB import LocalDB
 from Code.Model.Car.ParkingSlot import ParkingSlot
 from aiocoap import *
 from Code.Logging.Logger import loggerSetup
+
 import asyncio
 
 plateLogger = loggerSetup("plateLogger_PlateManager", "Code/Logging/Plate/plate.log")
@@ -18,7 +17,6 @@ sps = SendParkingStatus()
 
 BASE_URI = 'coap://127.0.0.1:5683/'
 
-logger = loggerSetup("plateLogger","Code/Logging/Plate/plate.log")
 
 class PlateManager:
     """This class allows to manage the incoming messages from parking considering entry/exit/parking status update.
@@ -29,6 +27,7 @@ class PlateManager:
     def __init__(self):
         self.mqttClient = None
         self.mqttBrokerParameters = None
+        self.configurations()
 
 
     def configurations(self):
@@ -37,8 +36,8 @@ class PlateManager:
         self.mqttBrokerParameters.fromJson(configparser)
         self.mqttBrokerParameters.idClient = "PlateManager"
         self.mqttClient = mqtt.Client(self.mqttBrokerParameters.idClient)
-        self.mqttClient.on_connect = lambda client, userdata, flags, rc: self.on_connect(client, userdata, flags, rc)
-        self.mqttClient.on_message = lambda client, userdata, msg: self.on_message(client, userdata, msg)
+        self.mqttClient.on_connect = PlateManager.on_connect
+        self.mqttClient.on_message = PlateManager.on_message
         self.mqttClient.username_pw_set(self.mqttBrokerParameters.USERNAME,
                                         self.mqttBrokerParameters.PASSWORD)
         self.mqttClient.connect(self.mqttBrokerParameters.BROKER_ADDRESS,
@@ -46,16 +45,19 @@ class PlateManager:
         self.mqttClient.loop_forever()
 
     @staticmethod
-    def on_connect(self, client, userdata, flags, rc):
-        devices_topic = "{0}/{1}/{2}/{3}/#".format(  # topic generico per ora
-            self.mqttBrokerParameters.BASIC_TOPIC,
-            self.mqttBrokerParameters.USERNAME,
-            self.mqttBrokerParameters.DEVICE_TOPIC,
-            self.mqttBrokerParameters.LOCATION
+    def on_connect(client, userdata, flags, rc):
+        mqttBrokerParameters = MQTTBrokerParameters()
+        configparser = open('Configuration/PlateReaderMQTTParameters/config.json')
+        mqttBrokerParameters.fromJson(configparser)
+        devices_topic = "{0}/{1}/{2}/#".format(  # topic generico per ora
+            mqttBrokerParameters.BASIC_TOPIC,
+            mqttBrokerParameters.USERNAME,
+            mqttBrokerParameters.DEVICE_TOPIC
         )
-        self.mqttClient.subscribe(devices_topic)
+        client.subscribe(devices_topic)
 
-        print(self.mqttBrokerParameters.idClient + " subscribed to: " + devices_topic)
+        # print(self.mqttBrokerParameters.idClient + " subscribed to: " + devices_topic)
+        plateLogger.info(mqttBrokerParameters.idClient + " subscribed to: " + devices_topic)
 
     @staticmethod
     def on_message(client, userdata, msg):
@@ -125,8 +127,6 @@ class PlateManager:
                 TODO: put/post to the monitor parking slots free and the nearest place
                         reading from the json
                 """
-                valid, availables, firstId = localParkingDBManager.checkParkingSlots()
-                
                 jsonData = json.loads(message_payload)
                 if jsonData["car"]["licensePlate"] == "":
                     parkingSlot = ParkingSlot(jsonData['parkingPlace'],
@@ -176,30 +176,28 @@ class PlateManager:
     def stop(self):
         self.stop()
 
-    @staticmethod
-    async def put_message(self, URI, text):
-        logging.basicConfig(level=logging.INFO)
-        protocol = await Context.create_client_context()
-        request = Message(code=aiocoap.Code.PUT, payload=text, uri=URI)
-        try:
-            response = await protocol.request(request).response
-        except Exception as e:
-            print(print('Failed to fetch resource: '))
-            print(e)
-        else:
-            print('Result: %s\n%r' % (response.code, response.payload))
 
-    @staticmethod
-    async def post_message(URI):
-        logging.basicConfig(level=logging.INFO)
-        protocol = await Context.create_client_context()
-        request = Message(code=aiocoap.Code.POST, uri=URI)
-        try:
-            response = await protocol.request(request).response
-        except Exception as e:
-            print(print('Failed to fetch resource: '))
-            print(e)
-        else:
-            print('Result: %s\n%r' % (response.code, response.payload))
+async def put_message(URI, text):
+    protocol = await Context.create_client_context()
+    request = Message(code=aiocoap.Code.PUT, payload=text.encode('utf-8'), uri=URI)
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource: ')
+        print(e)
+    else:
+        if response is not None:
+            print('Result: %s\n%r' % (response.code, response.payload.decode("utf-8")))
 
 
+async def post_message(URI):
+    protocol = await Context.create_client_context()
+    request = Message(code=aiocoap.Code.POST, uri=URI)
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource: ')
+        print(e)
+    else:
+        if response is not None:
+            print('Result: %s\n%r' % (response.code, response.payload.decode("utf-8")))
