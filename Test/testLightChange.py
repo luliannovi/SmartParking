b5d81f7cfdbfa2452c7d1db60060538baac94025
allePansera <allepansera2001@gmail.com>
2023-02-04 16:00:51 +0100

    testLight Change 1 resource

--- Test/testLightChange.py
+++ Test/testLightChange.py
@@ -4,7 +4,7 @@ import random, time
 
 # send lumen received from sensor to LightManager
 # lumen sensor possible range [0,2000]
+brightnessSensorResource = BrightnessSensorResource(sensorId="id", description="description")
 while True:
-    brightnessSensorResource = BrightnessSensorResource(sensorId="id", description="description")
     brightnessSensorResource.brightnessUpdate(random.randrange(0, AmbientBrightness.ACCEPTED_MAX, 10))
     time.sleep(30)
